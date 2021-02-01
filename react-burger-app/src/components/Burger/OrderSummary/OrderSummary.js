453550c74f4632c3814237c055865f1515e4bb61
pchangfatt <paul.changfatt@oandp.com>
2021-01-31 22:57:11 -0500

    Added the logic for the order summary. Added the toolbar for the nav. Added the logo and the image for it.

--- react-burger-app/src/components/Burger/OrderSummary/OrderSummary.js
+++ react-burger-app/src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,6 +1,8 @@
 import React from "react";
+import Button from "../../UI/Button/Button";
 
 const orderSummary = (props) => {
+  const { price, purhcaseContinued, purchaseCanceled } = props;
   const ingredientSummary = Object.keys(props.ingredients).map((igKey) => {
     return (
       <li key={igKey}>
@@ -15,7 +17,16 @@ const orderSummary = (props) => {
       <h3>Your Order</h3>
       <p>A delicious burger with the follow ingredients:</p>
       <ul>{ingredientSummary}</ul>
+      <p>
+        <strong>Total Price: {price.toFixed(2)}</strong>
+      </p>
       <p>Continue to Checkout?</p>
+      <Button btnType="Danger" clicked={purchaseCanceled}>
+        CANCEL
+      </Button>
+      <Button btnType="Success" clicked={purhcaseContinued}>
+        CONTINUE
+      </Button>
     </>
   );
 };
