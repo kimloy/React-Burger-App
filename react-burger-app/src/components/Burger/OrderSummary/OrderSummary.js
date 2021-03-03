32fa6522e76cddf6bc5de332e60ad011d26e68c1
pchangfatt <paul.changfatt@oandp.com>
2021-03-03 00:14:49 -0500

    Change the folder structure and improve the render for the modal to make sure it only updates when the props changtes.

--- react-burger-app/src/components/Burger/OrderSummary/OrderSummary.js
+++ react-burger-app/src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,34 +1,37 @@
-import React from "react";
+import React, { Component } from "react";
 import Button from "../../UI/Button/Button";
 
-const orderSummary = (props) => {
-  const { price, purhcaseContinued, purchaseCanceled } = props;
-  const ingredientSummary = Object.keys(props.ingredients).map((igKey) => {
-    return (
-      <li key={igKey}>
-        {" "}
-        <span style={{ textTransform: "capitalize" }}>{igKey}</span>:{" "}
-        {props.ingredients[igKey]}
-      </li>
+class OrderSummary extends Component {
+  render() {
+    const ingredientSummary = Object.keys(this.props.ingredients).map(
+      (igKey) => {
+        return (
+          <li key={igKey}>
+            {" "}
+            <span style={{ textTransform: "capitalize" }}>{igKey}</span>:{" "}
+            {this.props.ingredients[igKey]}
+          </li>
+        );
+      }
     );
-  });
-  return (
-    <>
-      <h3>Your Order</h3>
-      <p>A delicious burger with the follow ingredients:</p>
-      <ul>{ingredientSummary}</ul>
-      <p>
-        <strong>Total Price: {price.toFixed(2)}</strong>
-      </p>
-      <p>Continue to Checkout?</p>
-      <Button btnType="Danger" clicked={purchaseCanceled}>
-        CANCEL
-      </Button>
-      <Button btnType="Success" clicked={purhcaseContinued}>
-        CONTINUE
-      </Button>
-    </>
-  );
-};
+    return (
+      <>
+        <h3>Your Order</h3>
+        <p>A delicious burger with the follow ingredients:</p>
+        <ul>{ingredientSummary}</ul>
+        <p>
+          <strong>Total Price: {this.props.price.toFixed(2)}</strong>
+        </p>
+        <p>Continue to Checkout?</p>
+        <Button btnType="Danger" clicked={this.props.purchaseCanceled}>
+          CANCEL
+        </Button>
+        <Button btnType="Success" clicked={this.props.purhcaseContinued}>
+          CONTINUE
+        </Button>
+      </>
+    );
+  }
+}
 
-export default orderSummary;
+export default OrderSummary;
--- react-burger-app/src/components/UI/Modal/Modal.js
+++ react-burger-app/src/components/UI/Modal/Modal.js
@@ -1,24 +1,29 @@
-import React from "react";
+import React, { Component } from "react";
 
 import classes from "./Modal.module.css";
 import Backdrop from "../Backdrop/Backdrop";
 
-const modal = (props) => {
-  const { show } = props;
-  return (
-    <>
-      <Backdrop show={show} clicked={props.modalClosed} />
-      <div
-        className={classes.Modal}
-        style={{
-          transform: show ? "translateY(0)" : "translateY(-100vh)",
-          opacity: show ? "1" : "0",
-        }}
-      >
-        {props.children}
-      </div>
-    </>
-  );
-};
+class Modal extends Component {
+  shouldComponentUpdate(nextProps, nextState) {
+    return nextProps.show !== this.props.show;
+  }
 
-export default modal;
+  render() {
+    return (
+      <>
+        <Backdrop show={this.props.show} clicked={this.props.modalClosed} />
+        <div
+          className={classes.Modal}
+          style={{
+            transform: this.props.show ? "translateY(0)" : "translateY(-100vh)",
+            opacity: this.props.show ? "1" : "0",
+          }}
+        >
+          {this.props.children}
+        </div>
+      </>
+    );
+  }
+}
+
+export default Modal;
