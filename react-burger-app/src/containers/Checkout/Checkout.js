691b45af149bc2465b79af1407b58d014ad93e77
pchangfatt <paul.changfatt@oandp.com>
2021-04-28 16:30:28 -0400

    added the fetch actions, and call to redux. Implemented it inside the Orders component.

--- react-burger-app/src/containers/Checkout/Checkout.js
+++ react-burger-app/src/containers/Checkout/Checkout.js
@@ -15,10 +15,15 @@ class Checkout extends Component {
   };
 
   render() {
+    console.log(this.props.purchased);
     let summary = <Redirect to="/" />;
     if (this.props.ings) {
+      const purchasedRedirect = this.props.purchased ? (
+        <Redirect to="/" />
+      ) : null;
       summary = (
         <div>
+          {purchasedRedirect}
           <CheckoutSummary
             ingredients={this.props.ings}
             checkoutCanelled={this.checkoutCanelledHandler}
@@ -38,6 +43,7 @@ class Checkout extends Component {
 const mapStateToProps = (state) => {
   return {
     ings: state.burgerBuilder.ingredients,
+    purchased: state.order.purchased,
   };
 };
 
