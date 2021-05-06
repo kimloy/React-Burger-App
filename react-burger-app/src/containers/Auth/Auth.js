1c20d82a06561512b8c351482591fa0fce7ca404
pchangfatt <paul.changfatt@oandp.com>
2021-05-05 22:32:38 -0400

    Created the logic for signup and sign in

--- react-burger-app/src/containers/Auth/Auth.js
+++ react-burger-app/src/containers/Auth/Auth.js
@@ -1,8 +1,10 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
 
 import Input from "../../components/UI/Input/Input";
 import Button from "../../components/UI/Button/Button";
 import classes from "./Auth.module.css";
+import * as actions from "../../store/actions/index";
 
 class Auth extends Component {
   state = {
@@ -36,6 +38,7 @@ class Auth extends Component {
         touched: false,
       },
     },
+    isSignedUp: true,
   };
 
   checkValidity(value, rules) {
@@ -84,6 +87,21 @@ class Auth extends Component {
     this.setState({ controls: updatedControls });
   };
 
+  submitHandler = (event) => {
+    event.preventDefault();
+    this.props.onAuth(
+      this.state.controls.email.value,
+      this.state.controls.password.value,
+      this.state.isSignedUp
+    );
+  };
+
+  switchAuthModeHandler = () => {
+    this.setState((prevState) => {
+      return { isSignedUp: !prevState.isSignedUp };
+    });
+  };
+
   render() {
     const formElementsArray = [];
     for (let key in this.state.controls) {
@@ -107,13 +125,23 @@ class Auth extends Component {
     ));
     return (
       <div className={classes.Auth}>
-        <form>
+        <form onSubmit={this.submitHandler}>
           {form}
           <Button btnType="Success">SUBMIT</Button>
         </form>
+        <Button clicked={this.switchAuthModeHandler} btnType="Danger">
+          SWITCH TO {this.state.isSignedUp ? "SIGNIN" : "SIGNUP"}
+        </Button>
       </div>
     );
   }
 }
 
-export default Auth;
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onAuth: (email, password, isSignup) =>
+      dispatch(actions.auth(email, password, isSignup)),
+  };
+};
+
+export default connect(null, mapDispatchToProps)(Auth);
