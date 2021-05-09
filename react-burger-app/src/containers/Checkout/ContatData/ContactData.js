f61c4cb0c3cb2bf56268cf3e1b29ad87e072da83
pchangfatt <paul.changfatt@oandp.com>
2021-05-08 20:52:07 -0400

    Implement a way to protect resources in the backend using auth

--- react-burger-app/src/containers/Checkout/ContatData/ContactData.js
+++ react-burger-app/src/containers/Checkout/ContatData/ContactData.js
@@ -111,7 +111,7 @@ class ContactData extends Component {
       orderData: formData,
     };
 
-    this.props.onOrderBurger(order);
+    this.props.onOrderBurger(order, this.props.token);
   };
 
   inputChangedHandler = (event, inputIdentifier) => {
@@ -207,12 +207,14 @@ const mapStateToProps = (state) => {
     ings: state.burgerBuilder.ingredients,
     price: state.burgerBuilder.totalPrice,
     loading: state.order.loading,
+    token: state.auth.token,
   };
 };
 
 const mapDispatchToProps = (dispatch) => {
   return {
-    onOrderBurger: (orderData) => dispatch(actions.purchaseBurger(orderData)),
+    onOrderBurger: (orderData, token) =>
+      dispatch(actions.purchaseBurger(orderData, token)),
   };
 };
 
