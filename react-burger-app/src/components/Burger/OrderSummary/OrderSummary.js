e509d558e61dd0dcc1504e8da6142aefb12c2734
pchangfatt <paul.changfatt@oandp.com>
2021-06-09 17:21:15 -0400

    More conversions from class => functional component

--- react-burger-app/src/components/Burger/OrderSummary/OrderSummary.js
+++ react-burger-app/src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,37 +1,33 @@
-import React, { Component } from "react";
+import React from "react";
 import Button from "../../UI/Button/Button";
 
-class OrderSummary extends Component {
-  render() {
-    const ingredientSummary = Object.keys(this.props.ingredients).map(
-      (igKey) => {
-        return (
-          <li key={igKey}>
-            {" "}
-            <span style={{ textTransform: "capitalize" }}>{igKey}</span>:{" "}
-            {this.props.ingredients[igKey]}
-          </li>
-        );
-      }
-    );
+const OrderSummary = (props) => {
+  const ingredientSummary = Object.keys(props.ingredients).map((igKey) => {
     return (
-      <>
-        <h3>Your Order</h3>
-        <p>A delicious burger with the follow ingredients:</p>
-        <ul>{ingredientSummary}</ul>
-        <p>
-          <strong>Total Price: {this.props.price.toFixed(2)}</strong>
-        </p>
-        <p>Continue to Checkout?</p>
-        <Button btnType="Danger" clicked={this.props.purchaseCanceled}>
-          CANCEL
-        </Button>
-        <Button btnType="Success" clicked={this.props.purhcaseContinued}>
-          CONTINUE
-        </Button>
-      </>
+      <li key={igKey}>
+        {" "}
+        <span style={{ textTransform: "capitalize" }}>{igKey}</span>:{" "}
+        {props.ingredients[igKey]}
+      </li>
     );
-  }
-}
+  });
+  return (
+    <>
+      <h3>Your Order</h3>
+      <p>A delicious burger with the follow ingredients:</p>
+      <ul>{ingredientSummary}</ul>
+      <p>
+        <strong>Total Price: {props.price.toFixed(2)}</strong>
+      </p>
+      <p>Continue to Checkout?</p>
+      <Button btnType="Danger" clicked={props.purchaseCanceled}>
+        CANCEL
+      </Button>
+      <Button btnType="Success" clicked={props.purhcaseContinued}>
+        CONTINUE
+      </Button>
+    </>
+  );
+};
 
 export default OrderSummary;
